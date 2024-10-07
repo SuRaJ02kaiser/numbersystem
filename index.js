9e8221c636c1a1583832a3b2dcbd1aebab05dad5
Suraj <Surajsinghnagrkoti2992003@gmail.com>
2024-10-07 12:32:00 +0530

    added the js file which finds the even and odd numbers from the array

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-let arr=[1,2,3,4,5]
-let even = ""
-let odd = ""
+let arr = [ 19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30,];
+  
+let even = "";
+let odd = "";
  
 for(let i=0;i<arr.length;i++){
     if(arr[i]%2 == 0)
-        even = even + arr[i]
+        even = even + arr[i] +","
 if(arr[i]%2 == 1){
-    odd = odd + arr[i]
+    odd = odd + arr[i] + ","
 }
 console.log("Even",even)
 console.log("Odd",odd)
