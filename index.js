3a1b3970435f883b5409714dbe99e0724f83cb4b
Suraj <Surajsinghnagrkoti2992003@gmail.com>
2024-10-07 18:51:31 +0530

    added the index.js file which finds the even and odd numbers in the array

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ for(let i=0;i<arr.length;i++){
 if(arr[i]%2 == 1){
     odd = odd + arr[i] + ","
 }
-console.log("Even",even)
-console.log("Odd",odd)
+console.log("Even :",even)
+console.log("Odd :",odd)
 }
 
